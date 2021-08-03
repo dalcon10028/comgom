2ba4f282e7b3f301f26556e66055e6faf7b6f638
yeon <dalcon10280@gmail.com>
2021-08-04 00:07:43 +0900

    feat: 메인화면 틀 잡기

---
+++ components/main/Notice.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-card width="100%" height="400px">
+      공지사항
+  </v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/user/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+      로그인페이지
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
