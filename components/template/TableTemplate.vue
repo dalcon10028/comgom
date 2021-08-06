ca21df8956471524ea820a5fdc0d101cb29bd2da
yeon <dalcon10280@gmail.com>
2021-08-06 14:54:46 +0900

    refactor: TableTemplate 시간표시 개선

--- components/template/TableTemplate.vue
+++ components/template/TableTemplate.vue
@@ -9,9 +9,15 @@
       class="elevation-1"
       @page-count="pageCount = $event"
     >
-        <template #[`item.title`]="{ item }">
-            <NuxtLink :to="`/post/${item.id}`">{{ item.title }}</NuxtLink>
-        </template>
+      <template #[`item.title`]="{ item }">
+        <v-btn class="no-background-hover px-0" text :ripple="false">
+          <strong :class="`${item.post_label.color}--text`">[{{ item.post_label.label }}]&nbsp;</strong>
+          {{ item.title }}
+        </v-btn>
+      </template>
+      <template #[`item.created_at`]="{ item }">
+        {{ item.created_at | formatDate }}
+      </template>
     </v-data-table>
     <div class="text-center pt-2">
       <v-pagination
@@ -24,56 +30,88 @@
 
 <script>
 export default {
-    props: {
-        posts: {
-            type: Array,
-            required: true
-        }
-    },
-    
-    data () {
-      return {
-        page: 1,
-        pageCount: 0,
-        itemsPerPage: 10,
-        headers: [
-          {
-            text: '번호',
-            sortable: false,
-            value: 'id',
-          },
-          {
-            text: '제목',
-            sortable: false,
-            value: 'title',
-          },
-          {
-            text: '작성자',
-            sortable: false,
-            value: 'author.username',
-          },
-          {
-              text: '날짜',
-            sortable: false,
-            value: 'created_at',
-          },
-          {
-            text: '조회수',
-            sortable: false,
-            value: 'view',
-          },
-          
-          {
-            text: '좋아요',
-            sortable: false,
-            value: 'like',
-          },
-        ],
+
+  filters: {
+    formatDate(timestamp) {
+      const now = new Date();
+      const date = new Date(timestamp);
+      const betweenTime = Math.floor((now.getTime() - date.getTime()) / 1000 / 60);
+      if (betweenTime < 1) return '방금전';
+      if (betweenTime < 60) {
+          return `${betweenTime}분전`;
       }
-}
+      const betweenTimeHour = Math.floor(betweenTime / 60);
+      if (betweenTimeHour < 24) {
+          return `${betweenTimeHour}시간전`;
+      }
+      const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
+      if (betweenTimeDay < 3) {
+          return `${betweenTimeDay}일전`;
+      }
+      let month = '' + (date.getMonth() + 1);
+      let day = '' + date.getDate();
+      const year = date.getFullYear();
+
+      if (month.length < 2) 
+        month = '0' + month;
+      if (day.length < 2) 
+        day = '0' + day;
+
+      return [year, month, day].join('.');
+    }
+  },
+  props: {
+    posts: {
+        type: Array,
+        required: true
+    }
+  },
+  
+  data () {
+    return {
+      page: 1,
+      pageCount: 0,
+      itemsPerPage: 10,
+      headers: [
+        {
+          text: '번호',
+          sortable: false,
+          value: 'id',
+        },
+        {
+          text: '제목',
+          sortable: false,
+          value: 'title',
+        },
+        {
+          text: '작성자',
+          sortable: false,
+          value: 'author.username',
+        },
+        {
+            text: '날짜',
+          sortable: false,
+          value: 'created_at',
+        },
+        {
+          text: '조회수',
+          sortable: false,
+          value: 'view',
+        },
+        
+        {
+          text: '좋아요',
+          sortable: false,
+          value: 'like',
+        },
+      ],
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.no-background-hover::before {
+   background-color: transparent !important;
+}
 </style>
