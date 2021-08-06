2ffeefbcc4a9c275ec70838dc5402e5794b5e925
yeon <dalcon10280@gmail.com>
2021-08-06 14:51:58 +0900

    feat: 검색창 UI 추가

--- components/header/Tool.vue
+++ components/header/Tool.vue
@@ -1,8 +1,39 @@
 <template>
     <section>
-        <v-btn icon color="white">
+        <v-dialog
+      v-model="dialog"
+      width="500"
+    >
+      <template #activator="{ on, attrs }">
+        <v-btn 
+            icon 
+            color="white"  
+          v-bind="attrs"
+          v-on="on"
+          >
             <v-icon>mdi-magnify</v-icon>
         </v-btn>
+      </template>
+      <v-form
+        ref="form"
+        v-model="valid"
+        lazy-validation
+        >
+        <v-text-field
+            v-model="keyword"
+            filled
+            flat
+            :rules="keywordRules"
+            label="검색어를 입력하세요"
+            required
+            solo
+            background-color="white"
+            prepend-inner-icon="mdi-magnify"
+            hide-details
+            @keydown.enter="validate"
+        ></v-text-field>
+        </v-form>
+    </v-dialog>
         <v-btn v-if="!isAuthenticated" rounded color="white" class="primary--text"  @click="$router.push('/user/login')">
             로그인
         </v-btn>
@@ -21,9 +52,26 @@
 import { mapGetters } from 'vuex'
 
 export default {
+    data() {
+        return {
+            dialog: false,
+            valid: true,
+            keyword: '',
+            keywordRules: [ v => !!v ]
+        }
+    },
+
     computed: {
         ...mapGetters(["isAuthenticated", "loggedInUser"])
     },
+    
+    methods: {
+        validate () {
+            if (!this.$refs.form.validate()) return ;
+            this.$router.push(`/search/${this.keyword}`)
+            this.dialog = false;
+        },
+    }
 }
 </script>
 
---
+++ pages/search/_keyword.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+      <h1>검색</h1>
+      <h2>{{ $route.params.keyword }}</h2>
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
