30018b5eb7a142e6549ddb3e4f3bcac808921ab3
yeon <dalcon10280@gmail.com>
2021-08-07 00:12:57 +0900

    refactor: Not fouund page 개선

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,14 +1,17 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+  <v-app dark class="text-center">
+    <v-sheet class="pa-3" outlined rounded="lg">
+      <template v-if="error.statusCode === 404">
+        <v-img src="/404.png" max-height="300"></v-img>
+        <h1> {{ pageNotFound }} </h1>
+      </template>
+      <h1 v-else>
+        {{ otherError }}
+      </h1>
+      <NuxtLink to="/">
+        홈으로
+      </NuxtLink>
+    </v-sheet>
   </v-app>
 </template>
 
@@ -23,8 +26,8 @@ export default {
   },
   data () {
     return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
+      pageNotFound: '페이지를 찾을 수가 없대림..ㅠ',
+      otherError: '뭔가 문제가 있대림..ㅠ'
     }
   },
   head () {
