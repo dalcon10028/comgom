d4f8464ba2312334679f279f58d6f74defc2974e
yeon <dalcon10280@gmail.com>
2021-08-07 06:19:21 +0900

    refactor: navbar 아이콘 추가

--- components/header/Menu.vue
+++ components/header/Menu.vue
@@ -11,20 +11,21 @@
           v-on="on"
           @click="$router.push(`/${board.code}`)"
         >
+          <v-icon small>mdi-{{ board.icon }}</v-icon>&nbsp;
           {{ board.name }}
         </v-btn>
       </template>
 
       <v-list>
-          <v-list-item-group>
-            <v-list-item
-              v-for="(menu, idx) in board.boards"
-              :key="idx"
-              @click="$router.push(`/${board.code}/${menu.code}`)"
-            >
-            <v-list-item-title>{{ menu.name }}</v-list-item-title>
-            </v-list-item>
-          </v-list-item-group>
+        <v-list-item-group>
+          <v-list-item
+            v-for="(menu, idx) in board.boards"
+            :key="idx"
+            @click="$router.push(`/${board.code}/${menu.code}`)"
+          >
+          <v-list-item-title>{{ menu.name }}</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
       </v-list>
     </v-menu>
 </template>
