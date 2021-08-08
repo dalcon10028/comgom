3d0c6ec39eec89795acfb9ef647803130e4d7ddb
yeon <dalcon10280@gmail.com>
2021-08-09 01:14:32 +0900

    refactor: 게시물 제목 사이에 라인 넣기

--- components/main/Posts.vue
+++ components/main/Posts.vue
@@ -3,22 +3,24 @@
     <v-list>
       <v-subheader>자유게시판</v-subheader>
       <v-list-item-group>
-        <v-list-item
-          v-for="(post, i) in posts"
-          :key="i"
-          nuxt
-          dense
-          :to="`/post/${post.id}`"
-        >
-          <v-list-item-content>
-            <v-list-item-title>{{ post.title | titleDots }}</v-list-item-title>
-          </v-list-item-content>
-          <v-list-item-icon>
-            <span class="grey--text">{{ post.author.nickname }}</span>&nbsp;
-            <v-icon small color="error">mdi-heart</v-icon>&nbsp;
-            <span class="error--text">{{ post.like || 0 }}</span>
-          </v-list-item-icon>
-        </v-list-item>
+        <template v-for="(post, i) in posts">
+          <v-divider :key="i"></v-divider>
+          <v-list-item
+            :key="i"
+            nuxt
+            dense
+            :to="`/post/${post.id}`"
+          >
+            <v-list-item-content>
+              <v-list-item-title>{{ post.title | titleDots }}</v-list-item-title>
+            </v-list-item-content>
+            <v-list-item-icon>
+              <span class="grey--text">{{ post.author.nickname }}</span>&nbsp;
+              <v-icon small color="error">mdi-heart</v-icon>&nbsp;
+              <span class="error--text">{{ post.like || 0 }}</span>
+            </v-list-item-icon>
+          </v-list-item>
+        </template>
       </v-list-item-group>
     </v-list>
   </v-sheet>
